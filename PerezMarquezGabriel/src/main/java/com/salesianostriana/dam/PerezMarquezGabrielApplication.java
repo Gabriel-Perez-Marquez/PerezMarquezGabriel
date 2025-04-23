4d3c137c524dcb48962b016eae08d5afd754a45d
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2025-04-23 11:37:47 +0200

    Proyecto Final Empezado

---
+++ PerezMarquezGabriel/src/main/java/com/salesianostriana/dam/PerezMarquezGabrielApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerezMarquezGabrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerezMarquezGabrielApplication.class, args);
+	}
+
+}
