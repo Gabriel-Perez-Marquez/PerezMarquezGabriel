e212343cacfd158cd53d09b3d8ae2896c77d541f
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2025-04-28 12:30:54 +0200

    Algunas vistas y alguna funcionalidades inlcuidas

---
+++ PerezMarquezGabriel/src/main/java/com/salesianostriana/dam/controller/CatalogoController.java
@@ -0,0 +1,20 @@
+package com.salesianostriana.dam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CatalogoController {
+	
+	@GetMapping("/catalogo")
+	public String goToCatalogo() {
+		return "catalogo";
+	}
+	
+	@GetMapping("/add-new-room")
+	public String addNewRoom(String categorias, Model model) {
+		model.addAttribute("categorias", categorias);
+		return "new-room-form";
+	}
+}
--- PerezMarquezGabriel/src/main/java/com/salesianostriana/dam/controller/MainController.java
+++ PerezMarquezGabriel/src/main/java/com/salesianostriana/dam/controller/MainController.java
@@ -1,5 +1,17 @@
 package com.salesianostriana.dam.controller;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class MainController {
+	
+	
+	@GetMapping("/")
+	public String welcome() {
+		return "index";
+	}
+	
+	
+	
 }
